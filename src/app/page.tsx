33f72fd90f71bb16601db17790765ffb695d48db
tolor <269049637@qq.com>
2024-06-14 01:48:42 +0800

    你的提交信息

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
-import { Container, Grid, Paper, Typography, Box } from "@mui/material";
+import { Container, Grid, Paper, Typography, Box, Button, List, ListItem, ListItemText,Table,TableBody,TableHead,TableCell,TableRow, TableContainer,} from "@mui/material";
+
 
 const Dashboard = () => {
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-      <Typography variant="h4" align="center" gutterBottom>
-        定海派出所
-      </Typography>
-      <Grid container spacing={3}>
+    <Container sx={{ width: "100vw", height: "100vh", padding: 0 }}>
+    <Grid container spacing={3} alignItems="flex-start" justifyContent="flex-start">
         {/* 左侧面板 */}
         <Grid item xs={12} md={4}>
           <Paper
@@ -15,39 +13,191 @@ const Dashboard = () => {
               p: 2,
               display: "flex",
               flexDirection: "column",
-              height: "100%",
+              height: "30vh",
+              backgroundColor: "rgba(255,255,255,0.0)",
+              boxShadow: "none",
+              border: "2px solid white",
+              color: "white",
+              overflow: "hidden",
             }}
           >
-            <Typography variant="h6">勤务管理</Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="h6">勤务管理</Typography>
+              <Typography sx={{ ml: 2, fontSize: '1rem' }}>今日备勤等级:</Typography>
+              <Button variant="contained" color="primary" sx={{ ml: 'auto' }}>
+                编辑
+              </Button>
+            </Box>
+
+              <TableContainer component={Paper} sx= {{ backgroundColor: 'transparent', marginTop: '20px',overflow: 'hidden',minWidth: '100%',boxShadow: "none",}}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '80px', minWidth: '50px',whiteSpace: 'nowrap', overflow: 'hidden', }}>154654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>454446546545465465464654654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>Status</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                <TableRow>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '80px', minWidth: '50px',whiteSpace: 'nowrap', overflow: 'hidden', }}>154654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>454446546545465465464654654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>Status</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '80px', minWidth: '50px',whiteSpace: 'nowrap', overflow: 'hidden', }}>154654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>454446546545465465464654654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>Status</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '80px', minWidth: '50px',whiteSpace: 'nowrap', overflow: 'hidden', }}>154654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>454446546545465465464654654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>Status</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '80px', minWidth: '50px',whiteSpace: 'nowrap', overflow: 'hidden', }}>154654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>454446546545465465464654654654</TableCell>
+                    <TableCell sx={{ borderBottom: 'none', padding: '4px',color: 'white',textOverflow: 'ellipsis', maxWidth: '120px',minWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', }}>Status</TableCell>
+                  </TableRow>
+                </TableBody>
+              </TableContainer>
             {/* Add your content here */}
           </Paper>
-          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 }}>
-            <Typography variant="h6">街面巡逻</Typography>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 ,backgroundColor: "rgba(255,255,255,0.0)",
+          boxShadow: "none",border: "2px solid white",color: "white", height: "30vh",}}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="h6">街面巡逻</Typography>
+              <Typography sx={{ ml: 2, fontSize: '1rem' }}>今日备勤车辆:</Typography>
+              <Button variant="contained" color="primary" sx={{ ml: 'auto' }}>
+                编辑
+              </Button>
+            </Box>
+            <Box sx={{ mt: 2}}>
+              <Grid container spacing={1} >
+                  <Grid item xs = {2}>
+                    <Typography sx={{lineHeight: '1.2'}} ></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>早</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>中</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>晚</Typography>
+                  </Grid>
+                  <Grid item xs = {2}>
+                    <Typography sx={{lineHeight: '1.2'}} ></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>4587</Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2',overflow: 'hidden',}}>张三 李四</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>中</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>晚</Typography>
+                  </Grid>
+                  <Grid item xs = {2}>
+                    <Typography sx={{lineHeight: '1.2'}} ></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>早</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>中</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>晚</Typography>
+                  </Grid>
+                  <Grid item xs = {2}>
+                    <Typography sx={{lineHeight: '1.2'}} ></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>早</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>中</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>晚</Typography>
+                  </Grid>
+                  <Grid item xs = {2}>
+                    <Typography sx={{lineHeight: '1.2'}} ></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>早</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>中</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>晚</Typography>
+                  </Grid>
+                  <Grid item xs = {2}>
+                    <Typography sx={{lineHeight: '1.2'}} ></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}></Typography>
+                  </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>早</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>中</Typography>
+                    </Grid>
+                  <Grid item xs = {2.5}>
+                    <Typography sx={{lineHeight: '1.2'}}>晚</Typography>
+                  </Grid>
+              </Grid>
+            </Box>
             {/* Add your content here */}
           </Paper>
-          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 }}>
-            <Typography variant="h6">工作重点</Typography>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 ,backgroundColor: "rgba(255,255,255,0.0)",
+          boxShadow: "none",border: "2px solid white",color: "white", height: "30vh",}}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="h6">工作重点</Typography>
+              <Button variant="contained" color="primary" sx={{ ml: 'auto' }}>
+                编辑
+              </Button>
+            </Box>
             {/* Add your content here */}
           </Paper>
         </Grid>
 
         {/* 中间地图 */}
+        
         <Grid item xs={12} md={4}>
+          <Typography variant="h4" align="center" gutterBottom>
+            定海派出所
+          </Typography>
           <Box
-            sx={{
-              p: 2,
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img
-              src="/source/bg.jpg"
-              alt="地图"
-              style={{ maxWidth: "100%", maxHeight: "100%" }}
-            />
-          </Box>
+          component="img"
+          src="/source/bg.jpg"
+          alt="地图"
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            zIndex: -1,
+          }}
+          />
         </Grid>
 
         {/* 右侧面板 */}
@@ -57,18 +207,37 @@ const Dashboard = () => {
               p: 2,
               display: "flex",
               flexDirection: "column",
-              height: "100%",
+              height: "30vh",
+              backgroundColor: "rgba(255,255,255,0.0)",
+              boxShadow: "none",
+              border: "2px solid white",
+              color: "white",
+              
             }}
           >
-            <Typography variant="h6">警情分析</Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="h6">警情分析</Typography>
+              <Button variant="contained" color="primary" sx={{ ml: 'auto' }}>
+                编辑
+              </Button>
+            </Box>
             {/* Add your content here */}
           </Paper>
-          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 }}>
-            <Typography variant="h6">近期勤务</Typography>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 ,backgroundColor: "rgba(255,255,255,0.0)",
+          boxShadow: "none",border: "2px solid white",color: "white", height: "30vh",}}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="h6">近期勤务</Typography>
+              <Button variant="contained" color="primary" sx={{ ml: 'auto' }}>
+                编辑
+              </Button>
+            </Box>
             {/* Add your content here */}
           </Paper>
-          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 }}>
-            <Typography variant="h6">民警-警情</Typography>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column", mt: 2 ,backgroundColor: "rgba(255,255,255,0.0)",
+          boxShadow: "none",border: "2px solid white",color: "white", height: "30vh",}}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="h6">民警-警情</Typography>
+            </Box>
             {/* Add your content here */}
           </Paper>
         </Grid>
