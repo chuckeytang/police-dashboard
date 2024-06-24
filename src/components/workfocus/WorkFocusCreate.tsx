3a31f4a970069422890fe1173a22218d31ac6d17
tjq19842003 <chuckeytang@gmail.com>
2024-06-24 12:23:28 +0800

    可以跳转的create范例

--- src/components/workfocus/WorkFocusCreate.tsx
+++ src/components/workfocus/WorkFocusCreate.tsx
@@ -1,9 +1,25 @@
-import { Create, SimpleForm, TextInput, DateInput } from "react-admin";
+import React from "react";
+import { Create, SimpleForm, TextInput, DateInput, useRedirect, useNotify } from "react-admin";
+import { useDataProvider } from 'react-admin';
 
 const WorkFocusCreate = () => {
+  const redirect = useRedirect();
+  const notify = useNotify();
+  const dataProvider = useDataProvider();
+
+  const handleSuccess = async (data: any) => {
+    try {
+      await dataProvider.create('workFocus', { data });
+      notify('提交成功');
+      redirect('list', 'workFocus');
+    } catch (error) {
+      notify('提交失败');
+    }
+  };
+
   return (
     <Create>
-      <SimpleForm>
+      <SimpleForm onSubmit={handleSuccess}>
         <DateInput source="focus_date" label="日期" />
         <TextInput source="content" label="内容" />
       </SimpleForm>
